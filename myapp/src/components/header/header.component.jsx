2d33d8ccf3278936000db3636ad650074283734b
HO HUYNH THANH NAM <nam.t171846@vanlanguni.vn>
2020-10-06 20:36:47 +0700

    create header

---
+++ myapp/src/components/header/header.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.style.scss";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+const Header = () => (
+  <div className="header">
+    <Link to="/" className="logo-container">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link to="/shop" className="option">
+        Shop
+      </Link>
+      <Link to="/shop" className="option">
+        Contact
+      </Link>
+    </div>
+  </div>
+);
+export default Header;

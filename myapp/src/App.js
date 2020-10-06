71e32104606eb9aa3aeca73bd107431e5c016031
HO HUYNH THANH NAM <nam.t171846@vanlanguni.vn>
2020-10-06 18:39:52 +0700

    done

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
-import Homepage from "./pages/homepage/homepage.components.jsx";
+import Homepage from "./pages/homepage/homepage.components";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 function App() {
   return (
-    <div className="App">
-      <Homepage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
